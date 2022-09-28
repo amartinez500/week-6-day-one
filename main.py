2c4ab203e499dfe13815f4b1241da52fa6e804a7
Arturo Martinez <113930370+amartinez500@users.noreply.github.com>
2022-09-28 17:41:34 +0000

    Logical Operators practices

--- main.py
+++ main.py
@@ -98,28 +98,28 @@ print(my_bool)
 # Create three variables (num1, num2, and num3):
 
 # Inside num1, store the value 36
-
+num1 = 36
 # Inside num2, stores the result of the operation 72/2
-
+num2 = 72/2
 # Inside num3, store the value 48
-
+num3 = 48
 # Check if num1 is greater than num2, and less than num3. Store the result of that comparison in a variable called my_bool.
-
-
+my_bool = num1 > num2 and num1 < num3
+print(my_bool)
 
 
 # Logical Operators Practice #2
 # Create three variables (num1, num2, and num3):
 
 # Inside num1, store the value 36
-
+num1 = 36
 # Inside num2, stores the result of the operation 72/2
-
+num2 = 72/2
 # Inside num3, store the value 48
-
+num3 = 48
 # Check if num1 is greater than num2, or less than num3. Store the result of that comparison in a variable called my_bool.
-
-
+my_bool = num1 > num2 or num1 < num3 
+print(my_bool)
 
 # Logical Operators Practice #3
 # Check if the words:
